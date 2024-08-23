6295dee01a12fd773e16157fd89e779ee4f24517
Ruby Udders <89067407+RubyUdders@users.noreply.github.com>
2024-08-23 14:43:13 +0200

    Add files via upload

---
+++ demo/src/test/java/com/auth0_rwa_spring/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth0_rwa_spring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
